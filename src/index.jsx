cdb21447a044413504e5bdb8b14ac23075b5c603
NOTAM <sawyerbobk563@gmail.com>
2026-05-22 11:47:15 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
+// This is the entry point that injects your React App component into the index.html file
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
